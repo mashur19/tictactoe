bc12876e09fe3907b1497d3488951c922eb0afd0
mashur <mashur191200@gmail.com>
2019-10-18 22:45:28 +0530

    added player choice functionality to play the game

--- tictactoe.py
+++ tictactoe.py
@@ -10,6 +10,31 @@ def disBoard():
     print('  '+theBoard['bl'] +'  |  ' + theBoard['bc']+'  |  ' +theBoard['br'])
     print('--------------------')
 
+#check for winning condition 
+def winCondition():
+    if ((theBoard['tl']=='X' and theBoard['tc']=='X' and theBoard['tr']=='X') 
+     or (theBoard['ml']=='X' and theBoard['mc']=='X' and theBoard['mr']=='X')
+     or (theBoard['bl']=='X' and theBoard['bc']=='X' and theBoard['br']=='X')
+     or (theBoard['tl']=='X' and theBoard['ml']=='X' and theBoard['bl']=='X')
+     or (theBoard['tc']=='X' and theBoard['mc']=='X' and theBoard['bc']=='X')
+     or (theBoard['tr']=='X' and theBoard['mr']=='X' and theBoard['br']=='X')
+     or (theBoard['tl']=='X' and theBoard['mc']=='X' and theBoard['br']=='X')
+     or (theBoard['tr']=='X' and theBoard['mc']=='X' and theBoard['bl']=='X')):
+        return 1
+
+    elif ((theBoard['tl']=='O' and theBoard['tc']=='O' and theBoard['tr']=='O') 
+     or (theBoard['ml']=='O' and theBoard['mc']=='O' and theBoard['mr']=='O')
+     or (theBoard['bl']=='O' and theBoard['bc']=='O' and theBoard['br']=='O')
+     or (theBoard['tl']=='O' and theBoard['ml']=='O' and theBoard['bl']=='O')
+     or (theBoard['tc']=='O' and theBoard['mc']=='O' and theBoard['bc']=='O')
+     or (theBoard['tr']=='O' and theBoard['mr']=='O' and theBoard['br']=='O')
+     or (theBoard['tl']=='O' and theBoard['mc']=='O' and theBoard['br']=='O')
+     or (theBoard['tr']=='O' and theBoard['mc']=='O' and theBoard['bl']=='O')):
+        return 2
+
+    else:
+        return 0
+
 #def main:
 
 #ask the player to start the game 
@@ -30,49 +55,30 @@ if playChoice.lower() == 'y':
     print('player'+str(player)+' is X'+' and player'+str(nextPlayer)+' is O')
     disBoard()
     for i in range(9):
-        print('player'+str(player)+'enter the position for X')
-        capturePos(input())
+        while winCondition() == 0:
+            print('player'+str(player)+'enter the position for X')
+            c1=input().lower()
+            theBoard[c1]='X'
+            disBoard()
+            print('player'+str(nextPlayer)+'enter the position for O')
+            c2=input().lower()
+            theBoard[c2]='O'
+            disBoard()
+        break
 
-        disBoard()
-
+    if winCondition() == 1:
+        print('player'+str(player)+'wins the game')
+    else:
+        print('player'+str(nextplayer)+'wins the game')    
 else:
     SystemExit
-
+'''
 #capture move made by the player
 def capturePos(str):
     if str.isaplha == True:
-        theBoard['str']='X'
+        theBoard[str]='X'
     elif str.isalpha == True:
-        theBoard['str']='O'
-
-
-
-#check for winning condition 
-def winCondition():
-    if ((theBoard['tl']=='X' and theBoard['tc']=='X' and theBoard['tr']=='X') 
-     or (theBoard['ml']=='X' and theBoard['mc']=='X' and theBoard['mr']=='X')
-     or (theBoard['bl']=='X' and theBoard['bc']=='X' and theBoard['br']=='X')
-     or (theBoard['tl']=='X' and theBoard['ml']=='X' and theBoard['bl']=='X')
-     or (theBoard['tc']=='X' and theBoard['mc']=='X' and theBoard['bc']=='X')
-     or (theBoard['tr']=='X' and theBoard['mr']=='X' and theBoard['br']=='X')
-     or (theBoard['tl']=='X' and theBoard['mc']=='X' and theBoard['br']=='X')
-     or (theBoard['tr']=='X' and theBoard['mc']=='X' and theBoard['bl']=='X')):
-        return True
-
-    elif ((theBoard['tl']=='O' and theBoard['tc']=='O' and theBoard['tr']=='O') 
-     or (theBoard['ml']=='O' and theBoard['mc']=='O' and theBoard['mr']=='O')
-     or (theBoard['bl']=='O' and theBoard['bc']=='O' and theBoard['br']=='O')
-     or (theBoard['tl']=='O' and theBoard['ml']=='O' and theBoard['bl']=='O')
-     or (theBoard['tc']=='O' and theBoard['mc']=='O' and theBoard['bc']=='O')
-     or (theBoard['tr']=='O' and theBoard['mr']=='O' and theBoard['br']=='O')
-     or (theBoard['tl']=='O' and theBoard['mc']=='O' and theBoard['br']=='O')
-     or (theBoard['tr']=='O' and theBoard['mc']=='O' and theBoard['bl']=='O')):
-        return True
-
-    else:
-        return False    
-
-
-
+        theBoard[str]='O'
+'''
 
 
